5833ab49f6d000e7afd914e32716962adc266b34
Tom Hanna <tlhanna@uh.edu>
2026-01-20 01:14:22 -0600

    lab test and intro to r

---
+++ Labs/loop_example_1.R
@@ -0,0 +1,18 @@
+blue <- c(1,2,3,4,5,6,7,8,9,10)
+
+blue2 <- c()
+
+for (i in 1:10) {
+        blue2 <- c(blue2,blue*1)
+}
+print(blue2)
+
+
+
+vector1 <- c(1,2,3,4,5)
+
+for (i in 1:10) {
+        print(vector1^i)
+}
+
+
