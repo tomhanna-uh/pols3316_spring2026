ed7bee059733769d5888cb4c4e80d9987a7d88b0
Tom Hanna <tlhanna@uh.edu>
2026-03-09 23:12:09 +0000

    lab_4.R gitignore

---
+++ Labs/Lab_4/lab_4.R
@@ -0,0 +1,104 @@
+# load tidyverse in case needed
+
+library(tidyverse)
+
+# You will need to load the datasets we used in the last two labs by whichever method
+# worked for you - see the Lab 2 and 3 instructions and the Canvas Announcement 
+# https://canvas.uh.edu/courses/27445/pages/lab-3-error-fixes
+
+# after the data is loaded...
+
+
+# find the correlation and covariance for exports and polyarchy from v-dem
+# find the correlation and covariance for lifeExp and pop from gapminder_data
+
+# get variable names for the three files
+
+colnames(gapminder_data)
+
+colnames(vdem_indices_2014)
+
+colnames(my_data_loaded)
+
+
+
+# look at the help for the cov() function
+
+?cov
+
+?cor
+
+# get the covariances and correlations and store them as 1 for the vdem
+# and 2 for the gapminder
+
+cov1 <- cov(vdem_indices_2014$exports, vdem_indices_2014$polyarchy,
+            use = "pairwise.complete.obs")
+cor1 <- cor(vdem_indices_2014$exports, vdem_indices_2014$polyarchy,
+            use = "pairwise.complete.obs")
+
+cov2 <- cov(gapminder_data$lifeExp, gapminder_data$pop,
+            use = "pairwise.complete.obs")
+cor2 <- cor(gapminder_data$lifeExp, gapminder_data$pop,
+            use = "pairwise.complete.obs")
+
+
+# create a data frame with the results
+
+
+
+stats_df <- data.frame(
+        Dataset = c("V-Dem 2014", "Gapminder"),
+        Covariance = c(cov1, cov2),
+        Correlation = c(cor1, cor2)
+)
+
+# produce a table from the dataframe using stargazer
+
+library(stargazer)
+
+stargazer(stats_df,
+          summary = FALSE,
+          rownames = FALSE,
+          type = "text")  # or "text"/"html"
+
+
+## correlation matrix of V-dem data (vdem_indices_2014) in table
+
+# if needed - remove the comment from the install.packages, run it, and either delete
+# or recomment it
+install.packages("corrplot")
+
+library(dplyr)
+library(corrplot)
+
+# we have to remove the non-numeric variables to avoid errors
+
+# keep only numeric variables
+vdem_num <- vdem_indices_2014 %>% 
+        select(where(is.numeric))
+
+# correlation matrix (Pearson, pairwise complete obs)
+cor_mat <- cor(vdem_num, use = "pairwise.complete.obs", method = "pearson")
+
+# view as a table in console
+round(cor_mat, 2)
+
+stargazer(cor_mat, type = "text")
+
+
+pairs(vdem_num)
+
+# basic correlogram
+corrplot(cor_mat)
+
+# slightly nicer: only upper triangle, clustered
+corrplot(
+        cor_mat,
+        method = "color",
+        type   = "upper",
+        order  = "hclust",
+        addCoef.col = "black",   # add correlation coefficients
+        tl.col = "black",        # text label color
+        tl.srt = 45              # text rotation
+)
+
